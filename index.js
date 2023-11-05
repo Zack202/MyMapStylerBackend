f49aee21f882afa8e112f80ae0b12f3c26b6d9f9
Zachary Loiwnger <zackxbl4@gmail.com>
2023-11-05 13:39:21 -0500

    test

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose'); // Import mongoose
+
+// CREATE OUR SERVER
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+// SETUP THE MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
+
+// SETUP OUR OWN ROUTERS AS MIDDLEWARE
+const authRouter = require('./routes/auth-router');
+app.use('/auth', authRouter);
+const playlistsRouter = require('./routes/map-router');
+app.use('/api', playlistsRouter);
+
+// Connect to MongoDB Atlas using mongoose
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB Atlas');
+});
+
+// PUT THE SERVER IN LISTENING MODE
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ routes/map-router.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const MapController = require('../controllers/map-controller')
+const router = express.Router()
+const auth = require('../auth')
+
+
+module.exports = router
