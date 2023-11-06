9ecca62e008fa7e54cc67fe1d59d087cbaca9e16
Zachary Loiwnger <zackxbl4@gmail.com>
2023-11-06 13:58:28 -0500

    typo in index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: [
     'http://localhost:3000', 
-    'https://my-map-styler-frontend-60bea3c51be3.herokuapp.com/', 
+    'https://my-map-styler-frontend-60bea3c51be3.herokuapp.com', 
   ],
   credentials: true,
 }));
